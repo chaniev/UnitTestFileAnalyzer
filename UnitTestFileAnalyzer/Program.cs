bc27c7e65bc47e7e19ce951e49c88f29fefe069a
chaniev <chaniev@gmail.com>
2017-03-12 15:24:37 +0300

    fix

--- UnitTestFileAnalyzer/Program.cs
+++ UnitTestFileAnalyzer/Program.cs
@@ -14,34 +14,10 @@ namespace UnitTestFileAnalyzer
     {
         static void Main(string[] args)
         {
-            string filecontent = @"
-[TestClass]
-public class Sample
-{
-   public string FooProperty {get; set;}
-   
-   [TestMethod]
-   public void FooMethod()
-   {
-   }
-
-   [TestMethod]
-   public void FooMethod_Test()
-   {
-   }
-
-}";
             var sp = @"C:\Users\Sony\Documents\Visual Studio 2015\Projects\UnitTestNameDiagnosticAnalyzer\UnitTestNameDiagnosticAnalyzer.sln";
             var pac = new ProjectAnalyzerCollection(sp);
             pac.Analyze();
             pac.Save(Console.WriteLine);
-            //var classAnalyzer=new ClassAnalyzer(filecontent);
-            //classAnalyzer.Parse();
-            //Console.WriteLine("Incorrect method names");
-            //foreach (var methodname in classAnalyzer.Error)
-            //{
-            //    Console.WriteLine(methodname);
-            //}
             Console.ReadLine();
         }
 
